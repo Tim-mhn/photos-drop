350d3f07ebaba3f193c4034b98873696a33f3859
Timothy Meehan <tim.meehan097@gmail.com>
2023-06-30 17:24:21 +0200

    simplified structure and dropped core lib atm

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { CoreModule } from '../libs/core/src/core.module';
+import { ImagesModule } from './images/module';
 import { ImagesController } from './images/images.controller';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
@@ -8,7 +8,7 @@ import { isAuthMiddleware } from './auth.middleware';
 const rootPath = join(__dirname, '..', 'images');
 @Module({
   imports: [
-    CoreModule,
+    ImagesModule,
     ServeStaticModule.forRoot({
       rootPath: rootPath,
     }),
