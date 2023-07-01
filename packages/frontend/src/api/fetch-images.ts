ceeda29317907f60d604e06fa52c38eae0e1de89
Timothy Meehan <tim.meehan097@gmail.com>
2023-07-01 11:06:03 +0200

    used env files for api endpoints

--- packages/frontend/src/api/fetch-images.ts
+++ packages/frontend/src/api/fetch-images.ts
@@ -1,6 +1,7 @@
 import { APIClient } from "./client";
 import { Images } from "@shared";
+import { IMAGES_API_ENDPOINT } from "./endpoints";
 
 export async function fetchImages(client: APIClient) {
-  return client.get<Images>("http://localhost:8000/images");
+  return client.get<Images>(IMAGES_API_ENDPOINT);
 }
--- packages/frontend/src/api/upload-images.ts
+++ packages/frontend/src/api/upload-images.ts
@@ -1,5 +1,6 @@
 import { QueryClient } from "@tanstack/vue-query";
 import { APIClient } from ".";
+import { UPLOAD_API_ENDPOINT } from "./endpoints";
 
 export const IMAGES_QUERY_KEY = "IMAGES" as const;
 export async function uploadImages(
@@ -8,12 +9,11 @@ export async function uploadImages(
   images: FileList,
 ) {
   const formData = buildFormDataFromFileList(images);
-  await client.post("http://localhost:8000/images/upload", formData);
+  await client.post(UPLOAD_API_ENDPOINT, formData);
   await queryClient.invalidateQueries({ queryKey: [IMAGES_QUERY_KEY] });
 }
 
 function buildFormDataFromFileList(fileList: FileList) {
-  console.log(fileList);
   const formData = new FormData();
   for (let i = 0; i < fileList.length; i++) {
     const f = fileList[i];
